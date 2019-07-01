4b78d325e2551fe2a712cfdb71aa3805d9d7dafe
Matheus Nícolas <matheuspb8@gmail.com>
2019-07-01 10:31:44 -0300

    Class snmp walk implemented

---
+++ snmp_walk.py
@@ -0,0 +1,25 @@
+from easysnmp import Session
+from easysnmp import snmp_get, snmp_set, snmp_walk 
+
+class Walk:
+    
+    def __init__(self, variable, hostname, community, version):
+        self.variable = variable
+        self.hostname = hostname
+        self.community = community
+        self.version = version
+    
+    def _walk(self):
+        session = Session(hostname=self.hostname, community=self.community, version=self.version)
+        system_items = session.bulkwalk(self.variable)
+        for item in system_items:
+            print '{oid}.{oid_index} {snmp_type} = {value}'.format(
+              oid=item.oid,
+              oid_index=item.oid_index,
+              snmp_type=item.snmp_type,
+              value=item.value
+            )
+
+if __name__ == '__main__':
+    walk = Walk('node', '000.000.0.0:0000', 'public', 2)
+    walk._walk()
